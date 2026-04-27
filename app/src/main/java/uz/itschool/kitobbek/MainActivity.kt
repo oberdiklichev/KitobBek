ffe1a96d9654acb3ffc003fdb2be38ca7e707e18
Berdiklichev Og'abek <oberdiklichev@gmail.com>
2026-04-27 09:35:58 +0500

    commit-1

--- app/src/main/java/uz/itschool/kitobbek/MainActivity.kt
+++ app/src/main/java/uz/itschool/kitobbek/MainActivity.kt
@@ -13,7 +13,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import uz.itschool.kitobbek.ui.theme.KitobBekTheme
 
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
